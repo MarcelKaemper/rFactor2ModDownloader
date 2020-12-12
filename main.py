060ee436ef90dbbb62ecec4c646e8d3d718f76ec
Marcel Kämper <marcelkaemper862@gmail.com>
2020-12-12 14:32:16 +0100

    Added linux version and comments

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ steamPath = "D:\Programs\SteamCmd"
 serverPackagesPath = ".\Packages"
 scriptDir = os.path.dirname(os.path.abspath(__file__))
 
-urls = input("Please enter the URLs of the workshop items you want to download (separated by comma):\n$ ").split(",")
+urls = input("Please enter the URLs or the IDs of the workshop items you want to download (separated by comma):\n$ ").split(",")
 
 print(urls)
 
 ids = [] 
 
+# Convert urls to ids
 for url in urls:
     ids.append(re.sub("\D", "", url))
 
 print(ids)
 
-#for id in ids:
-#    os.system(steamPath+"\steamcmd.exe +force_install_dir "+scriptDir+" +login anonymous +workshop_download_item 365960 "+id+" +quit")
+# Download .rfcmp files
+for id in ids:
+    os.system(steamPath+"\steamcmd.exe +force_install_dir "+scriptDir+" +login anonymous +workshop_download_item 365960 "+id+" +quit")
 
 rfcmpPath = ".\steamapps\workshop\content\\365960\\" 
 
+# Move to Serverdir
 for id in ids:
     for file in os.listdir(rfcmpPath+id+"\\"):
         print(file)
         shutil.move(rfcmpPath+id+"\\"+file, serverPackagesPath)
-
-
-
---
+++ modDownloader.py
@@ -0,0 +1,28 @@
+import os
+import re
+import shutil
+
+steamPath = "./steamcmd"
+serverPackagesPath = "./rfactor2-dedicated/Packages"
+scriptDir = os.path.dirname(os.path.abspath(__file__))
+
+urls = input("Please enter the URLs of the workshop items you want to download (separated by comma):\n$ ").split(",")
+
+print(urls)
+
+ids = [] 
+
+for url in urls:
+    ids.append(re.sub("\D", "", url))
+
+print(ids)
+
+for id in ids:
+    os.system("wine "+steamPath+"/steamcmd.exe +force_install_dir .. +login anonymous +workshop_download_item 365960 "+id+" +quit")
+
+rfcmpPath = "./steamapps/workshop/content/365960/" 
+
+for id in ids:
+   for file in os.listdir(rfcmpPath+id+"/"):
+       print(file)
+       shutil.move(rfcmpPath+id+"/"+file, serverPackagesPath)
